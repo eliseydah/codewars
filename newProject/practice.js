793926726e36bbb1d9bdc4577baea5594bd156ed
Daria Eliseeva <elisey422das@gmail.com>
2025-01-16 11:52:01 +0100

    update names

--- newProject/practice.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector("button");
-let text = document.querySelector("input");
-let textWindow = document.querySelector("div");
-function showText() {
-  textWindow.innerHTML = text.value;
-  text.value = " ";
-}
-button.addEventListener("click", showText);
