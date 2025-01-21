8e7e8e1b6beb16768865cd042af4bdfff8a337dd
Daria Eliseeva <elisey422das@gmail.com>
2025-01-21 14:09:52 +0100

    resolve a task

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
-function calculateSubtraction(a, b) {
-  if (b > a) {
-    return b - a;
-  }
-}
-
-let romanObj = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000,
-};
-
-function solution(roman) {
-  let romanArray = roman.split("");
-  let numberArray = [];
-  for (let i = 0; i < romanArray.length; i++) {
-    if (
-      calculateSubtraction(romanObj[romanArray[i]], romanObj[romanArray[i + 1]])
-    ) {
-      numberArray.push(
-        calculateSubtraction(
-          romanObj[romanArray[i]],
-          romanObj[romanArray[i + 1]]
-        )
-      );
-      i++;
-    } else {
-      numberArray.push(romanObj[romanArray[i]]);
+function expandedForm(num) {
+  let newString = "";
+  let finalArray = [];
+  let newArray = num.toString().split("");
+  for (let i = 0; i < newArray.length; i++) {
+    let a = newArray.length - i - 1;
+    if (newArray[i] !== "0") {
+      finalArray.push(newArray[i] * Math.pow(10, a));
     }
   }
-  return numberArray.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-  }, 0);
+
+  return finalArray.join(" + ");
 }
-
-console.log(solution("XXI")); //21
-console.log(solution("IV")); // 4
-console.log(solution("XIX"));
-console.log(solution("MMVIII")); //2008
-console.log(solution("MDCLXVI")); //   1666
-console.log(solution("MCMLXXXVI")); // 1986
-
-// Symbol    Value
-// I          1
-//V          5
-// X          10
-//L          50
-//C          100
-// D          500
-// M          1,000
+console.log(expandedForm(12)); // "10+2"
+console.log(expandedForm(42)); // "40+2"
+console.log(expandedForm(70304)); // "70000 + 300 + 4"
+console.log(expandedForm(12));
